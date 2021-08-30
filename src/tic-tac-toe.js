8e29bf47908ca56f24a9bbaae6da92b18e8fa627
Max Tyshkevich <2842778@gmail.com>
2021-08-30 23:37:05 +0300

    feat: add metod's of class

--- src/tic-tac-toe.js
+++ src/tic-tac-toe.js
@@ -1,35 +1,64 @@
 class TicTacToe {
-    constructor() {
+  constructor() {}
+  currentPlayer = 'x';
+  gameField = [
+    [null, null, null],
+    [null, null, null],
+    [null, null, null],
+  ];
+  getCurrentPlayerSymbol() {
+    return this.currentPlayer;
+  }
 
+  nextTurn(rowIndex, columnIndex) {
+    if (this.gameField[rowIndex][columnIndex]) return;
+    this.gameField[rowIndex][columnIndex] = this.currentPlayer;
+    this.currentPlayer = this.currentPlayer === 'x' ? 'o' : 'x';
+  }
+
+  isFinished() {
+    return !!this.getWinner() || this.noMoreTurns();
+  }
+  getWinner() {
+    let matrix = this.gameField;
+    if (matrix[0][0] == matrix[0][1] && matrix[0][1] == matrix[0][2])
+      return matrix[0][0];
+    if (matrix[1][0] == matrix[1][1] && matrix[1][1] == matrix[1][2])
+      return matrix[1][0];
+    if (matrix[2][0] == matrix[2][1] && matrix[2][1] == matrix[2][2])
+      return matrix[2][0];
+
+    if (matrix[0][0] == matrix[1][0] && matrix[1][0] == matrix[2][0])
+      return matrix[0][0];
+    if (matrix[0][1] == matrix[1][1] && matrix[1][1] == matrix[2][1])
+      return matrix[0][1];
+    if (matrix[0][2] == matrix[1][2] && matrix[1][2] == matrix[2][2])
+      return matrix[0][2];
+
+    if (matrix[0][0] == matrix[1][1] && matrix[1][1] == matrix[2][2])
+      return matrix[1][1];
+    if (matrix[0][2] == matrix[1][1] && matrix[1][1] == matrix[2][0])
+      return matrix[1][1];
+
+    return null;
+  }
+
+  noMoreTurns() {
+    for (let i = 0; i < this.gameField.length; i++) {
+      if (this.gameField[i].includes(null)) {
+        return false;
+      }
     }
+    return true;
+  }
 
-    getCurrentPlayerSymbol() {
+  isDraw() {
+    return this.noMoreTurns() && !this.getWinner();
+  }
 
-    }
-
-    nextTurn(rowIndex, columnIndex) {
-
-    }
-
-    isFinished() {
-
-    }
-
-    getWinner() {
-
-    }
-
-    noMoreTurns() {
-
-    }
-
-    isDraw() {
-
-    }
-
-    getFieldValue(rowIndex, colIndex) {
-
-    }
+  getFieldValue(rowIndex, colIndex) {
+    return this.gameField[rowIndex][colIndex];
+  }
 }
 
 module.exports = TicTacToe;
